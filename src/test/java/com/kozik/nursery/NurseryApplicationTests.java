8bb5de701904aa7cf2fe0d72d2facaaddf7238ba
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-04 12:22:45 +0100

    Init new repository

---
+++ src/test/java/com/kozik/nursery/NurseryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kozik.nursery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
