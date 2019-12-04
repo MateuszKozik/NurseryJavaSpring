340514cc6aac3ca5bad89e3f69db04138d5a9e7c
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-04 16:25:39 +0100

    Added employee entity and employee repository

---
+++ src/main/java/com/kozik/nursery/Employee.java
@@ -0,0 +1,149 @@
+package com.kozik.nursery;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity()
+@Table(name = "emplyee")
+public class Employee {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "emplyeeID", nullable = false)
+    private Integer emplyeeID;
+    
+    @Column(name = "name", nullable = false, length = 25)
+    private String name;
+    
+    @Column(name = "surname", nullable = false, length = 25)
+    private String surname;
+    
+    @Column(name = "employment_date", nullable = false)
+    private LocalDate emplymentDate;
+    
+    @Column(name = "phone_number", nullable = false, length = 9)
+    private String phoneNumber;
+    
+    @Column(name = "position", nullable = true, length = 25)
+    private String position;
+    
+    @Column(name = "base_salary", nullable = true)
+    private Double baseSalary;
+    
+    @Column(name = "extra_pay", nullable = true)
+    private Double extraPay;
+    
+    @ManyToOne
+    @JoinColumn(name = "supervisor", nullable = true)
+    private Employee supervisor;
+    
+    @OneToMany 
+    private Set<Employee> subordinates = new HashSet<Employee>();
+
+    protected Employee(){}
+
+    public Employee(String name, String surname, LocalDate emplymentDate, String phoneNumber) {
+        this.name = name;
+        this.surname = surname;
+        this.emplymentDate = emplymentDate;
+        this.phoneNumber = phoneNumber;
+    }
+    
+    
+    public Integer getEmplyeeID() {
+        return emplyeeID;
+    }
+
+    public void setEmplyeeID(Integer emplyeeID) {
+        this.emplyeeID = emplyeeID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public LocalDate getEmplymentDate() {
+        return emplymentDate;
+    }
+
+    public void setEmplymentDate(LocalDate emplymentDate) {
+        this.emplymentDate = emplymentDate;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+    public Double getExtraPay() {
+        return extraPay;
+    }
+
+    public void setExtraPay(Double extraPay) {
+        this.extraPay = extraPay;
+    }
+
+    public Employee getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Employee supervisor) {
+        this.supervisor = supervisor;
+    }
+
+    public Set<Employee> getSubordinates() {
+        return subordinates;
+    }
+
+    public void setSubordinates(Set<Employee> subordinates) {
+        this.subordinates = subordinates;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" + "emplyeeID=" + emplyeeID + ", name=" + name + ", surname=" + surname + ", emplymentDate=" + emplymentDate.toString() + ", phoneNumber=" + phoneNumber + ", position=" + position + ", baseSalary=" + baseSalary + ", extraPay=" + extraPay + ", supervisor=" + supervisor + " }";
+    }
+    
+    
+}
--- src/main/java/com/kozik/nursery/NurseryApplication.java
+++ src/main/java/com/kozik/nursery/NurseryApplication.java
@@ -1,5 +1,6 @@
 package com.kozik.nursery;
 
+import java.time.LocalDate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -14,11 +15,34 @@ public class NurseryApplication implements CommandLineRunner{
 
     @Autowired
     private UserRepository userRepository;
+    @Autowired
+    private EmployeeRepository employeeRepository;
         
     @Override
     public void run(String... args)  {
+        
+        //test user entity
         userRepository.save(new User("test", "test", "test@test.com","ADMIN"));
-        System.out.println(userRepository.findAll());
+        for(User user: userRepository.findAll()){
+            System.out.println(user);
+        }
+        
+        //test employee entity
+        Employee supervisor = new Employee("Joseph", "Simmons", LocalDate.now(), "123456789");
+        employeeRepository.save(supervisor);
+        
+        Employee employee1 = new Employee("Samuel", "Griffin", LocalDate.now(), "123456789");
+        Employee employee2 = new Employee("Sebastian", "Bennett", LocalDate.now(), "123456789");
+           
+        employee1.setSupervisor(supervisor);
+        employee1.setBaseSalary(1500.00);
+        employeeRepository.save(employee1);
+        
+        employee2.setPosition("accountant");
+        employeeRepository.save(employee2);
+        for(Employee employee: employeeRepository.findAll()){
+            System.out.println(employee);
+        }
     }
 
 }
