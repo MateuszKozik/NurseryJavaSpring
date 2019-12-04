239bea4b4ff62b2fefbbdcfe811e10445774b168
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-04 14:56:30 +0100

    Added user entity and user repository

--- src/main/java/com/kozik/nursery/NurseryApplication.java
+++ src/main/java/com/kozik/nursery/NurseryApplication.java
@@ -1,13 +1,24 @@
 package com.kozik.nursery;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NurseryApplication {
+public class NurseryApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(NurseryApplication.class, args);
 	}
+
+    @Autowired
+    private UserRepository userRepository;
+        
+    @Override
+    public void run(String... args)  {
+        userRepository.save(new User("test", "test", "test@test.com","ADMIN"));
+        System.out.println(userRepository.findAll());
+    }
 
 }
---
+++ src/main/java/com/kozik/nursery/User.java
@@ -0,0 +1,107 @@
+
+package com.kozik.nursery;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "userID", nullable = false)
+    private Integer userID;
+    
+    @Column(name = "username", nullable = false, length = 25, unique = true)
+    private String userName;
+    
+    @Column(name = "password", nullable = false, length = 60)
+    private String password;
+    
+    @Column(name = "email", nullable = false, length = 35)
+    private String email;
+    
+    @Column(name = "roles", nullable = false, length = 25)
+    private String roles = "USER";
+    
+    @Column(name = "enabled", nullable = false)
+    private Boolean enabled = true; 
+
+    protected User(){}
+    
+       public User(String userName, String password, String email) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+       
+    public User(String userName, String password, String email, String roles) {
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+        this.roles = roles;
+    }
+
+    
+    public Integer getUserID() {
+        return userID;
+    }
+
+    public void setUserID(Integer userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRoles() {
+        return roles;
+    }
+
+    public void setRoles(String roles) {
+        this.roles = roles;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+    
+    @Override
+    public String toString() {
+        return "User{" + "userID=" + userID + ", userName=" + userName + ", password=" + password + ", email=" + email + ", roles=" + roles + ", enabled=" + enabled.toString() + '}';
+    }
+    
+  
+    
+    
+}
