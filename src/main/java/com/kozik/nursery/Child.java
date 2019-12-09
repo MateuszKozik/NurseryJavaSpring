fe7658edcd1cc7ffcb956a77db23f082d77c488f
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-09 11:16:29 +0100

    Added child entity and child repository

---
+++ src/main/java/com/kozik/nursery/Child.java
@@ -0,0 +1,79 @@
+
+package com.kozik.nursery;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "children")
+public class Child {
+   
+    @Id
+    @Column(name = "pesel", nullable = false, length = 11)
+    private String pesel;
+    
+    @Column(name = "name", nullable = false, length = 25)
+    private String name;
+    
+    @Column(name = "surname", nullable = false, length = 25)
+    private String surname;
+    
+    @Column(name = "special_requirements", nullable = true)
+    private Boolean specialRequirements;
+    
+    @Column(name = "requirements_description", nullable = true, length = 60)
+    private String requirementsDescription;
+
+    protected Child(){}
+
+    public Child(String pesel, String name, String surname) {
+        this.pesel = pesel;
+        this.name = name;
+        this.surname = surname;
+    }
+    
+    
+    public String getPesel() {
+        return pesel;
+    }
+
+    public void setPesel(String pesel) {
+        this.pesel = pesel;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public Boolean getSpecialRequirements() {
+        return specialRequirements;
+    }
+
+    public void setSpecialRequirements(Boolean specialRequirements) {
+        this.specialRequirements = specialRequirements;
+    }
+
+    public String getRequirementsDescription() {
+        return requirementsDescription;
+    }
+
+    public void setRequirementsDescription(String requirementsDescription) {
+        this.requirementsDescription = requirementsDescription;
+    }
+    
+    
+}
