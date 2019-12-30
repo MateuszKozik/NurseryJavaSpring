bae1b79264a4dc348fcebe6bca0d2a9447330936
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-30 10:22:23 +0100

    Moved config class to the /config folder

--- src/main/java/com/kozik/nursery/config/WebSecuityConfig.java
+++ src/main/java/com/kozik/nursery/config/WebSecuityConfig.java
@@ -1,5 +1,5 @@
 
-package com.kozik.nursery;
+package com.kozik.nursery.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,7 +14,7 @@ public class WebSecuityConfig extends WebSecurityConfigurerAdapter{
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
-                .antMatchers("/","/home").permitAll()
+                .antMatchers("/**").permitAll()
                 .anyRequest().permitAll();
     }
 }
