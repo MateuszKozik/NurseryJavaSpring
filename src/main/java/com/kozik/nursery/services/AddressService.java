f9261a8d143dd9f22a469e16fb8339d566ce987f
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-30 12:19:46 +0100

    Created address service

---
+++ src/main/java/com/kozik/nursery/services/AddressService.java
@@ -0,0 +1,30 @@
+package com.kozik.nursery.services;
+
+import com.kozik.nursery.entities.Address;
+import org.springframework.beans.factory.annotation.Autowired;
+import com.kozik.nursery.repositories.AddressRepository;
+import java.util.List;
+import java.util.Set;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+    
+    @Autowired private AddressRepository addressRepository;
+    
+    public List<Address> getAll(){
+        return addressRepository.findAll();
+    } 
+    
+    public void save(Address adress){
+        addressRepository.save(adress);
+    }
+    
+    public Address get(long id){
+        return addressRepository.findById(id).get();
+    }
+    
+    public void delete(long id){
+       addressRepository.deleteById(id);
+    }
+}
