78b77247916cf6e09ad73affc5d9f76fd7db3b14
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-31 13:33:29 +0100

    Created employee controller

---
+++ src/main/java/com/kozik/nursery/controllers/EmployeeController.java
@@ -0,0 +1,59 @@
+package com.kozik.nursery.controllers;
+
+import com.kozik.nursery.entities.Employee;
+import com.kozik.nursery.services.EmployeeService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class EmployeeController {
+    
+    @Autowired EmployeeService employeeService;
+    
+    @GetMapping(value = "/employee/list")
+    public String listAll(Model model){
+        List<Employee> employeeList = employeeService.getAll();
+        model.addAttribute("employeeList", employeeList);
+        return "views/employee/list";
+    }
+    
+    @GetMapping(value = "/emloyee/add")
+    public String add(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "views/employee/add";
+    }
+    
+    @PostMapping(value = "/employee/add")
+    public String add(@ModelAttribute("emloyee")Employee employee){
+        employeeService.save(employee);
+        return "redirect:/employee/list"; 
+    }
+    
+    @GetMapping(value = "/employee/edit/{id}")
+    public String edit(@PathVariable("id")long id, Model model){
+        Employee employee = employeeService.get(id);
+        model.addAttribute("employee", employee);
+        return "views/employee/edit";
+    }
+    
+    @PostMapping(value = "/emloyee/edit/{id}")
+    public String edit(@PathVariable("id")long id,
+            @ModelAttribute("employee")Employee employee){
+        employee.setEmployeeID(id);
+        employeeService.save(employee);
+        return "redirect:/employee/list";
+    }
+    
+    @GetMapping(value = "/employee/delete/{id}")
+    public String delete(@PathVariable("id")long id){
+        employeeService.delete(id);
+        return "redirect:/employee/list";
+    }
+}
