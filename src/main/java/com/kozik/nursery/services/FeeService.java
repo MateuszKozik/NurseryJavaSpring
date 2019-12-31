87122c29e2a63d36e235d4de97d078fc7b624e73
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-31 19:07:13 +0100

    Created fee service

---
+++ src/main/java/com/kozik/nursery/services/FeeService.java
@@ -0,0 +1,29 @@
+package com.kozik.nursery.services;
+
+import com.kozik.nursery.entities.Fee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.kozik.nursery.repositories.FeeRepository;
+import java.util.List;
+
+@Service
+public class FeeService {
+    
+    @Autowired FeeRepository feeRepository;
+    
+    public List<Fee> getAll(){
+        return feeRepository.findAll();
+    }
+    
+    public void save(Fee fee){
+        feeRepository.save(fee);
+    }
+    
+    public Fee get(long id){
+        return feeRepository.findById(id).get();
+    }
+    
+    public void delete(long id){
+        feeRepository.deleteById(id);
+    }
+}
