0562a3ae22dc797c0845611dc909de885174b57d
Mateusz Kozik <mateuszkozik100@gmail.com>
2019-12-31 15:42:24 +0100

    Created group service

--- src/main/java/com/kozik/nursery/services/GroupService.java
+++ src/main/java/com/kozik/nursery/services/GroupService.java
@@ -1,14 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.kozik.nursery.services;
 
-/**
- *
- * @author mateusz
- */
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.kozik.nursery.repositories.GroupRepository;
+import java.util.List;
+import com.kozik.nursery.entities.Group;
+
+@Service
 public class GroupService {
     
+    @Autowired private GroupRepository groupRepository;
+    
+    public List<Group> getAll(){
+        return groupRepository.findAll();
+    }
+    
+    public void save(Group group){
+        groupRepository.save(group);
+    }
+    
+    public Group get(long id){
+        return groupRepository.findById(id).get();
+    }
+    
+    public void delete(long id){
+        groupRepository.deleteById(id);
+    }
 }
