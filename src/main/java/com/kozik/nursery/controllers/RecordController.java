83e71c18afb4ab785a6d11f94b9899b1713d8246
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-01-01 19:20:07 +0100

    Added group and fee selection

--- src/main/java/com/kozik/nursery/controllers/RecordController.java
+++ src/main/java/com/kozik/nursery/controllers/RecordController.java
@@ -1,7 +1,11 @@
 package com.kozik.nursery.controllers;
 
+import com.kozik.nursery.entities.Fee;
+import com.kozik.nursery.entities.Group;
 import com.kozik.nursery.services.RecordService;
 import com.kozik.nursery.entities.Record;
+import com.kozik.nursery.services.FeeService;
+import com.kozik.nursery.services.GroupService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +14,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class RecordController {
     
     @Autowired private RecordService recordService;
+    @Autowired private FeeService feeService;
+    @Autowired private GroupService groupService;
     
     @GetMapping(value = "/record/list")
     public String listAll(Model model){
@@ -25,28 +32,44 @@ public class RecordController {
     
     @GetMapping(value = "/record/add")
     public String add(Model model){
+        List<Group> groupList = groupService.getAll();
+        List<Fee> feeList = feeService.getAll();
         Record record = new Record();
         model.addAttribute("record", record);
+        model.addAttribute("groupList", groupList);
+        model.addAttribute("feeList", feeList);
         return "views/record/add";
     }
     
     @PostMapping(value = "/record/add")
-    public String add(@ModelAttribute("record")Record record){
+    public String add(@ModelAttribute("record")Record record,
+            @RequestParam("group")Group group,
+            @RequestParam("fee")Fee fee){
+        record.setGroup(group);
+        record.setFee(fee);
         recordService.save(record);
         return "redirect:/record/list";
     }
     
     @GetMapping(value = "/record/edit/{id}")
     public String edit(@PathVariable("id")long id, Model model){
+        List<Group> groupList = groupService.getAll();
+        List<Fee> feeList = feeService.getAll();
         Record record = recordService.get(id);
         model.addAttribute("record", record);
+        model.addAttribute("groupList", groupList);
+        model.addAttribute("feeList", feeList);
         return "views/record/edit";
     }
     
     @PostMapping(value = "/record/edit/{id}")
     public String edit(@PathVariable("id")long id,
-            @ModelAttribute("record")Record record){
+            @ModelAttribute("record")Record record,
+            @RequestParam("group")Group group,
+            @RequestParam("fee")Fee fee){
         record.setRecordID(id);
+        record.setGroup(group);
+        record.setFee(fee);
         recordService.save(record);
         return "redirect:/record/list";
     }
