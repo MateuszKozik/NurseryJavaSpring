4028d71c6d8696b46359295308fca4e89d3951ba
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-01-01 19:49:32 +0100

    Added room selection

--- src/main/java/com/kozik/nursery/controllers/ItemController.java
+++ src/main/java/com/kozik/nursery/controllers/ItemController.java
@@ -1,7 +1,9 @@
 package com.kozik.nursery.controllers;
 
 import com.kozik.nursery.entities.Item;
+import com.kozik.nursery.entities.Room;
 import com.kozik.nursery.services.ItemService;
+import com.kozik.nursery.services.RoomService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +12,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ItemController {
     
     @Autowired private ItemService itemService;
+    @Autowired private RoomService roomService;
     
     @GetMapping(value = "/item/list")
     public String listAll(Model model){
@@ -25,28 +29,36 @@ public class ItemController {
     
     @GetMapping(value = "/item/add")
     public String add(Model model){
+        List<Room> roomList = roomService.getAll();
         Item item = new Item();
         model.addAttribute("item", item);
+        model.addAttribute("roomList", roomList);
         return "views/item/add";
     }
     
     @PostMapping(value = "/item/add")
-    public String add(@ModelAttribute("item")Item item){
+    public String add(@ModelAttribute("item")Item item,
+            @RequestParam("room")Room room){
+        item.setRoom(room);
         itemService.save(item);
         return "redirect:/item/list";
     }
     
     @GetMapping(value = "/item/edit/{id}")
     public String edit(@PathVariable("id")long id, Model model){
+        List<Room> roomList = roomService.getAll();
         Item item = itemService.get(id);
         model.addAttribute("item", item);
+        model.addAttribute("roomList", roomList);
         return "views/item/edit";
     }
     
     @PostMapping(value = "/item/edit/{id}")
     public String edit(@PathVariable("id")long id,
-            @ModelAttribute("item")Item item){
+            @ModelAttribute("item")Item item,
+            @RequestParam("room")Room room){
         item.setInventoryNumber(id);
+        item.setRoom(room);
         itemService.save(item);
         return "redirect:/item/list"; 
     }
