2081b3a764ac18ff9f3c1ed2e71ffd716f5b5c6a
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-01-01 18:48:11 +0100

    Added rooms and empolyees selected

--- src/main/java/com/kozik/nursery/controllers/GroupController.java
+++ src/main/java/com/kozik/nursery/controllers/GroupController.java
@@ -1,7 +1,12 @@
 package com.kozik.nursery.controllers;
 
+import com.kozik.nursery.entities.Employee;
 import com.kozik.nursery.services.GroupService;
 import com.kozik.nursery.entities.Group;
+import com.kozik.nursery.entities.Room;
+import com.kozik.nursery.services.EmployeeService;
+import com.kozik.nursery.services.RoomService;
+import java.util.HashSet;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -10,11 +15,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 
 @Controller
 public class GroupController {
  
-    @Autowired GroupService groupService;
+    @Autowired private GroupService groupService;
+    @Autowired private EmployeeService employeeService;
+    @Autowired private RoomService roomService;
     
     @GetMapping(value = "/group/list")
     public String listAll(Model model){
@@ -25,28 +34,44 @@ public class GroupController {
     
     @GetMapping(value = "/group/add")
     public String add(Model model){
+        List<Employee> employeeList = employeeService.getAll();
+        List<Room> roomList = roomService.getAll();
         Group group = new Group();
         model.addAttribute("group", group);
+        model.addAttribute("employeeList", employeeList);
+        model.addAttribute("roomList", roomList);
         return "views/group/add";
     }
     
     @PostMapping(value = "/group/add")
-    public String add(@ModelAttribute("group")Group group){
+    public String add(@ModelAttribute("group")Group group,
+            @RequestParam("employees")HashSet<Employee> employees,
+            @RequestParam("rooms")HashSet<Room> rooms){
+        group.setEmployees(employees);
+        group.setRooms(rooms);
         groupService.save(group);
         return "redirect:/group/list";
     }
     
     @GetMapping(value = "/group/edit/{id}")
     public String edit(@PathVariable("id")long id, Model model){
+        List<Employee> employeeList = employeeService.getAll();
+        List<Room> roomList = roomService.getAll();
         Group group = groupService.get(id);
         model.addAttribute("group", group);
+        model.addAttribute("employeeList", employeeList);
+        model.addAttribute("roomList", roomList);
         return "views/group/edit";
     }
     
     @PostMapping(value = "/group/edit/{id}")
     public String edit(@PathVariable("id")long id,
-            @ModelAttribute("group")Group group){
+            @ModelAttribute("group")Group group,
+            @RequestParam("employees")HashSet<Employee> employees,
+            @RequestParam("rooms")HashSet<Room> rooms){
         group.setGroupID(id);
+        group.setEmployees(employees);
+        group.setRooms(rooms);
         groupService.save(group);
         return "redirect:/group/list";
     }
