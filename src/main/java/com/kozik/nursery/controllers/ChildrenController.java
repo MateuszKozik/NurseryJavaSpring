d0675b3101383a741322a4477a7e2937627bb8f2
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-01-02 16:40:23 +0100

    Added children and parent exists veification

--- src/main/java/com/kozik/nursery/controllers/ChildrenController.java
+++ src/main/java/com/kozik/nursery/controllers/ChildrenController.java
@@ -40,9 +40,48 @@ public class ChildrenController {
     public String children(@RequestParam(name = "pesel", required = false) String pesel,
             Principal principal, Model model) {
         String email = principal.getName();
-        if (childService.isChildPresent(pesel)) {
-            Child child = childService.get(pesel);
+        if (parentService.isParentPresent(email)) {
+            if (childService.isChildPresent(pesel)) {
+                Child child = childService.get(pesel);
+                Parent parent = parentService.getByEmail(email);
+                List<Child> childList = childService.getByParent(parent);
+                Set<Child> children;
+                if (!childList.isEmpty()) {
+                    children = new HashSet<Child>(childList);
+                    children.add(child);
+                } else {
+                    children = new HashSet<Child>();
+                    children.add(child);
+                }
+                parent.setChildren(children);
+                parentService.save(parent);
+                return "redirect:/customer/children";
+            } else {
+                return "redirect:/customer/children/add";
+            }
+        } else {
+            model.addAttribute("profile", true);
+            return "views/customer/children";
+        }
+    }
+
+    @GetMapping(value = "/customer/children/add")
+    public String add(Model model) {
+        Child child = new Child();
+        model.addAttribute("child", child);
+        return "views/customer/addChildren";
+    }
+
+    @PostMapping(value = "/customer/children/add")
+    public String add(@ModelAttribute("child") Child child,
+            Principal principal, Model model) {
+        String email = principal.getName();
+        if (parentService.isParentPresent(email)) {
+            if (childService.isChildPresent(child.getPesel())) {
+                return "redirect:/customer/children";
+            }
             Parent parent = parentService.getByEmail(email);
+            childService.save(child);
             List<Child> childList = childService.getByParent(parent);
             Set<Child> children;
             if (!childList.isEmpty()) {
@@ -56,34 +95,8 @@ public class ChildrenController {
             parentService.save(parent);
             return "redirect:/customer/children";
         } else {
-            return "redirect:/customer/addChildren";
+            model.addAttribute("profile", true);
+            return "views/customer/children";
         }
-    }
-
-    @GetMapping(value = "/customer/addChildren")
-    public String add(Model model) {
-        Child child = new Child();
-        model.addAttribute("child", child);
-        return "views/customer/addChildren";
-    }
-
-    @PostMapping(value = "/customer/addChildren")
-    public String add(@ModelAttribute("child") Child child,
-            Principal principal) {
-        String email = principal.getName();
-        Parent parent = parentService.getByEmail(email);
-        childService.save(child);
-        List<Child> childList = childService.getByParent(parent);
-        Set<Child> children;
-        if (!childList.isEmpty()) {
-            children = new HashSet<Child>(childList);
-            children.add(child);
-        } else {
-            children = new HashSet<Child>();
-            children.add(child);
-        }
-        parent.setChildren(children);
-        parentService.save(parent);
-        return "redirect:/customer/children";
     }
 }
