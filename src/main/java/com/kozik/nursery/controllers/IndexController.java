ea488070d4b5d1af9d188ad06ac1354695cf958f
Mateusz Kozik <mateuszkozik100@gmail.com>
2020-01-03 16:07:14 +0100

    Created index page

--- src/main/java/com/kozik/nursery/controllers/IndexController.java
+++ src/main/java/com/kozik/nursery/controllers/IndexController.java
@@ -1,13 +1,22 @@
 package com.kozik.nursery.controllers;
 
+import com.kozik.nursery.entities.Fee;
+import com.kozik.nursery.services.FeeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
+    @Autowired
+    private FeeService feeService;
     
     @GetMapping(value = "/")
-    public String showHomePage(){
+    public String showHomePage(Model model){
+         Fee fee = feeService.getLast();
+    
+        model.addAttribute("fee", fee);
         return "/index";
     }
 }
